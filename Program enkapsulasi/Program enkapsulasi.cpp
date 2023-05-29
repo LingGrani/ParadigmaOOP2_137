09e10d5b367f8158cd1f2b9cc083261d66c0360d
LingGrani <luthfiangger2510@gmail.com>
2023-05-29 19:58:32 +0700

    fungsi yang dapat mengakses data

--- Program enkapsulasi/Program enkapsulasi.cpp
+++ Program enkapsulasi/Program enkapsulasi.cpp
@@ -4,4 +4,11 @@ using namespace std;
 class remoteLampu {
 private:
 	string saklarNo[10]; /*data yang hanya dapat diakses oleh member class sendiri*/
+public:
+	void setSaklarNo(int i, string value) {
+		saklarNo[i] = value;
+	}
+	string getSaklarNo(int i) {
+		return saklarNo[i];
+	}
 };
